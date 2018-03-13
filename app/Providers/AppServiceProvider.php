ac6dac72b55ce14cecc33f77bf513e61b2979143
MechanicPro <33151693+MechanicPro@users.noreply.github.com>
2018-03-13 19:49:05 +0300

    MechanicPro
    Create

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use App\Channel;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+
+        \View::composer('*', function ($view) {
+            $channels = Channel::all();
+            $view->with('channels', $channels);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ app/Thread.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    public function scopeFilter($query, ThreadFilters $filters)
+    {
+        return $filters->apply($query);
+    }
+}
